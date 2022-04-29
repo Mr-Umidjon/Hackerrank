a03ae2f1b79b3a6b659b3f067e70cbcbde4794b3
Umidjon <mrumidjon2020@gmail.com>
2022-04-29 12:12:45 +0500

    select all

---
+++ SQL/Select/select_all.sql
@@ -0,0 +1,20 @@
+--Query all columns (attributes) for every row in the CITY table.
+--The CITY table is described as follows:
+
+--      CITY
+--Field       Type
+--
+--ID          NUMBER
+--NAME        VARCHAR2(17)
+--COUNTRYCODE VARCHAR2(3)
+--DISTRICT    VARCHAR2 (20)
+--POPULATION  NUMBER
+
+select * from CITY;
+
+
+
+
+
+
+
