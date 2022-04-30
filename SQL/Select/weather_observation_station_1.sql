4cc979fa057f7f32cd747f2a2bac8822a4c08665
Umidjon <mrumidjon2020@gmail.com>
2022-04-30 10:46:44 +0500

    Weather Observation Station 1

---
+++ SQL/Select/weather_observation_station_1.sql
@@ -0,0 +1,14 @@
+--Query a list of CITY and STATE from the STATION table.
+--The STATION table is described as follows:
+
+--      STATION
+--
+-- FIELD         TYPE
+-- ID            NUMBER
+-- CITY          VARCHAR2(21)
+-- STATE         VARCHAR2(21)
+-- LAT_N         NUMBER
+-- LANG_W        NUMBER
+
+
+select CITY, STATE from STATION
