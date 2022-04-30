09ac93f889bad7bf693212826b3d985da0fc6768
Umidjon <mrumidjon2020@gmail.com>
2022-04-30 10:47:01 +0500

    Weather Observation Station 3

---
+++ SQL/Select/weather_observation_station_3.sql
@@ -0,0 +1,15 @@
+--Query a list of CITY names from STATION for cities that have an even ID number.
+--Print the results in any order, but exclude duplicates from the answer.
+--The STATION table is described as follows:
+
+--      STATION
+--
+-- FIELD         TYPE
+-- ID            NUMBER
+-- CITY          VARCHAR2(21)
+-- STATE         VARCHAR2(21)
+-- LAT_N         NUMBER
+-- LANG_W        NUMBER
+
+
+select CITY, STATE from STATION
