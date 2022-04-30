91c1323e62b47b7ea370285ae7479cd8ebdb26d0
Umidjon <mrumidjon2020@gmail.com>
2022-04-30 11:03:50 +0500

    Weather Observation Station 4

--- SQL/Select/weather_observation_station_4.sql
+++ SQL/Select/weather_observation_station_4.sql
@@ -14,7 +14,6 @@
 
 select count(CITY) - count(distinct CITY) from STATION;
 
-
 --where LAT_N is the northern latitude and LONG_W is the western longitude.
 --For example, if there are three records in the table with CITY values 'New York', 'New
 --York', 'Bengalaru', there are 2 different city names: 'New York' and 'Bengalaru'.
